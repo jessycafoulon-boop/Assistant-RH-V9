fcfd23bf16c7e955c17d8fc7dfcc5eaf525609e2
jessycafoulon-boop <jessyca.foulon@gmail.com>
2026-08-28 16:15:35 +0200

    Fix labels and formatting in CONTACTS object

--- app.js
+++ app.js
@@ -12,24 +12,24 @@ const ORG_NAME =
 
 const CONTACTS = {
 
-    emploicompetences:{
-    label:"Secteur Emploi et développement des compténces",
+emploicompetences:{
+    label:"Secteur Emploi et développement des compétences",
     email: " A compléter",
     ficheUrl:"https://c.conflans.mairie-conflans.fr/#!/community/Ressources%2520Humaines/b1876ea4-a47a-4cb4-b84e-076eeade8ce6/d8bad4a4-3557-4feb-8087-54df234e8080/e7504b01-45da-4aa1-8f7f-45d5f22e4ef0/viewdetail/"
   },
    
-    gestionnaireCarriere:{
+gestionnaireCarriere:{
     label:"Secteur Gestion des carrières",
     email:"rhgestion@mairie-conflans.fr",
     ficheUrl:"https://c.conflans.mairie-conflans.fr/#!/community/Ressources%2520Humaines/b1876ea4-a47a-4cb4-b84e-076eeade8ce6/d8bad4a4-3557-4feb-8087-54df234e8080/3d4b5cc5-67cf-4b73-aead-3c3c223bd917/viewdetail/"
   },
-      sante:{
+sante:{
     label:"Secteur Santé",
     email:"rhgestion@mairie-conflans.fr",
     ficheUrl:"https://c.conflans.mairie-conflans.fr/#!/community/Ressources%2520Humaines/b1876ea4-a47a-4cb4-b84e-076eeade8ce6/d8bad4a4-3557-4feb-8087-54df234e8080/3d4b5cc5-67cf-4b73-aead-3c3c223bd917/viewdetail/"
   },
 
-  rhPaie:{
+ rhPaie:{
     label:"Secteur Paie",
     email:"rhpaie@mairie-conflans.fr",
     ficheUrl:"https://c.conflans.mairie-conflans.fr/#!/community/Ressources%2520Humaines/b1876ea4-a47a-4cb4-b84e-076eeade8ce6/d8bad4a4-3557-4feb-8087-54df234e8080/562850b9-0548-4410-8029-832972da3089/viewdetail/"
@@ -50,14 +50,12 @@ const CONTACTS = {
   intranet:{
     label:"Secteur Intranet",
     email:"A_COMPLETER",
-    phone:"A_COMPLETER",
     ficheUrl:"https://c.conflans.mairie-conflans.fr/#!/community/Ressources%2520Humaines/b1876ea4-a47a-4cb4-b84e-076eeade8ce6/d8bad4a4-3557-4feb-8087-54df234e8080/7d3a9df8-a60b-468f-ae58-e32e601e0fd2/viewdetail/"
   },
 
   prevention:{
     label:"Secteur Prévention",
     email:"A_COMPLETER",
-    phone:"A_COMPLETER",
     ficheUrl:"https://c.conflans.mairie-conflans.fr/#!/community/Ressources%2520Humaines/b1876ea4-a47a-4cb4-b84e-076eeade8ce6/d8bad4a4-3557-4feb-8087-54df234e8080/14ce95ca-56d5-44f7-a316-f377e4e2960c/viewdetail/"
   }
 
